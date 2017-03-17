eb75bf4cfaddba46a78b9588a605ecdaab93dd65
duoergun0729 <250261953@qq.com>
2017-03-17 23:03:18 +0800

    svm code

--- etl.py
+++ etl.py
@@ -1,5 +1,11 @@
 import re
+import numpy as np
+from sklearn import cross_validation
+from sklearn import datasets
+from sklearn import svm
 
+x = []
+y = []
 
 
 def get_len(url):
@@ -19,7 +25,7 @@ def get_evil_word(url):
 
 
 
-def etl(filename):
+def etl(filename,data,isxss):
     try:
         file_object = open(filename)
         for line in file_object:
@@ -27,11 +33,21 @@ def etl(filename):
             f2=get_url_count(line)
             f3=get_evil_char(line)
             f4=get_evil_word(line)
-            if f3 > 0:
-                print line
+            data.append([f1,f2,f3,f4])
+            if isxss:
+                y.append(1)
+            else:
+                y.append(0)
     finally:
         file_object.close( )
+    return data
+
+etl('xss-10000.txt',x,1)
+etl('good-xss-10000.txt',x,0)
 
 
-etl('xss-200000.txt')
-#etl('good-xss-200000.txt')
+x_train, x_test, y_train, y_test = cross_validation.train_test_split(x,y, test_size=0.4, random_state=0)
+
+clf = svm.SVC(kernel='linear', C=1).fit(x_train, y_train)
+print clf.score(x_test, y_test)
+
